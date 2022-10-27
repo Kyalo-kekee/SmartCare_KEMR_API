a113a12f636d611529ddcdcea0396a7569b60731
Kekee <kekekuza@gmail.com>
2022-10-27 09:27:43 +0300

    Add project files.

---
+++ Api.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.MessageTypes.Common;
+using DataAccessLayer.MessageTypes;
+using Hangfire;
+namespace SmartCare;
+
+public static class Api
+{
+
+  
+    public static void ConfigureApi(this WebApplication app )
+    {
+        app.MapPost("/Patient", ProcessRequest);
+
+    }
+    
+    private static IResult ProcessRequest(AbstractPatientMessage patient, IProcessMessage repository)
+    {
+        MESSAGEHEADER msgHeader = patient.MESSAGE_HEADER;
+        msgHeader = msgHeader ?? throw new ArgumentNullException(nameof(msgHeader));
+
+        
+        if (msgHeader.MESSAGE_TYPE == "ADT^A04")
+        {
+            var jobId = BackgroundJob.Schedule(() => repository.RegisterPatient(patient),
+                TimeSpan.FromSeconds(3));
+        }
+
+        if(msgHeader.MESSAGE_TYPE == "RDE^001")
+        {
+           var job=  BackgroundJob.Schedule(() => repository.CreatePhamarcyOrder(patient),
+               TimeSpan.FromSeconds(3));
+
+            var job2 = BackgroundJob.Schedule(() => repository.PopulatePharmarcyOrder(patient),
+                TimeSpan.FromSeconds(3));
+        }
+        return Results.Ok(msgHeader);
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+using DataAccessLayer.Data;
+using DataAccessLayer.MessageTypes;
+using Hangfire;
+using Hangfire.SqlServer;
+using SmartCare;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IAbstractPatientMessage, AbstractPatientMessage>();
+builder.Services.AddSingleton<IPharmarcyOrders, PharmarcyOrders>();
+builder.Services.AddSingleton<IPatientInformation, PatientInformation>();
+builder.Services.AddSingleton<ICustomerCreateFromCcc, CustomerCreateFromCcc>();
+builder.Services.AddSingleton<IProcessMessage, ProcessMessage>();
+builder.Services.AddHangfire(x => x.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseColouredConsoleLogProvider()
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage(builder.Configuration.GetConnectionString("Default"), new SqlServerStorageOptions
+                {
+                    CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                    SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                    QueuePollInterval = TimeSpan.Zero,
+                    UseRecommendedIsolationLevel = true
+                }));
+builder.Services.AddHangfireServer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    
+}
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.ConfigureApi();
+app.UseHangfireDashboard();
+app.UseHttpsRedirection();
+
+
+app.Run();
+//call api.
